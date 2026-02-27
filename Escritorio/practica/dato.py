28dab1f2c5c5d77d972878b96b2b2214ecc84af2
gabriel2090 <gabrielpallares644@gmail.com>
2026-02-26 20:26:02 -0500

    prueba2

---
+++ Escritorio/practica/dato.py
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+
+#oijdfgjoisdfg
+#sdfgsdfg
+
+#sdfgs
+#sfgsfd
+
+datos = "Isaias"
+
+print(f"bienvenido {datos} ")
+
+=======
+datos = "Isaias"
+
+print(f"bienvenido {datos} ")
+>>>>>>> ba71e1a1351bc38f21398a9980c6536b9d1d014a
